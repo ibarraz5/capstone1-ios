b6542792b6abee4f5a4946aecd77a0e5c2518b89
Ivan <75654991+ibarraz5@users.noreply.github.com>
2022-11-11 21:40:12 -0700

    Add files via upload

---
+++ xcschemes/demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  demo1App.swift
+//  demo1
+//
+//  Created by Ivan Barraza Duran on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
